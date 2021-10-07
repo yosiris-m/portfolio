482a72828c2e9b2bfcc70e04c70d30b51dcb9f7a
Yosiris Mariñez <yosiris0510@gmail.com>
2021-10-07 10:12:48 +0200

    improve correction component

--- src/Component/main/Contact/Contact.js
+++ src/Component/main/Contact/Contact.js
@@ -4,7 +4,7 @@ function Contact() {
   return (
     <section className={styles.wrapper}>
       <p className={styles.msg}>
-        Si cres que puedo aportar aportar valor a tu empresa....{" "}
+        Si crees que puedo aportar aportar valor a tu empresa....{" "}
         <strong>¡hablemos! </strong> 😉
       </p>
       <p>
