618aff66a54f7f626f0b8da1057f5271301d40ee
Yosiris <yosiris0510@gmail.com>
2023-12-13 13:47:30 +0100

    refactor fiele

--- src/Component/main/AboutMe.js
+++ src/Component/main/AboutMe.js
@@ -13,11 +13,11 @@ function AboutMe() {
           <p>
            <span>Licenciada en Bioanálisis, tras varios años en el área de la sanidad
             decidí reinventarme al sector IT. Empecé con un Bootcamp intensivo dónde aprendí mis primeros pasos en programación.  </span> 
-            <span>Actualmente sigo formándome en un : </span> <i>Grado superior en Desarrollo de Aplicaciones Web.</i>
+            <span>Actualmente sigo formándome en un </span> <strong>Grado superior en Desarrollo de Aplicaciones Web.</strong>
           </p>
           <p>
            <span>En mi corta experiencia laboral en el sector he tenido la oportunidad de trabajar en HeyTrade(Startup sector inversiones)
-             y en SopraHR(Consultora de servicios HR) de la mano de grandes compañeros creando, refactorizando y actualizando código.  </span> 
+             y en SopraHR(Consultora de servicios HR) de la mano de grandes compañeros. </span> 
            <span>  Actualmente estoy buscando una oportunidad laboral en la que pueda construibuir y seguir creciendo en este mundillo.
            </span> 
           </p>
